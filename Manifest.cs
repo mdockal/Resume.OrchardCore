ccd5b7354ef96a86a9483c7dc915ea6aa755e499
Antoine Griffard <agriffard@hotmail.com>
2018-08-11 19:11:07 +0200

    First commit

---
+++ Manifest.cs
@@ -0,0 +1,9 @@
+using OrchardCore.DisplayManagement.Manifest;
+
+[assembly: Theme(
+    Name = "Resume.OrchardCore",
+    Author = "The Orchard Team",
+    Website = "https://orchardproject.net",
+    Version = "0.0.1",
+    Description = "Resume.OrchardCore"
+)]
