ecba07f3c1aa957d9b3be067a10b0b4736d61607
Antoine Griffard <agriffard@hotmail.com>
2018-08-13 09:34:05 +0200

    no message

--- Manifest.cs
+++ Manifest.cs
@@ -1,9 +1,9 @@
 using OrchardCore.DisplayManagement.Manifest;
 
 [assembly: Theme(
-    Name = "Resume.OrchardCore",
+    Name = "Resume",
     Author = "The Orchard Team",
     Website = "https://orchardproject.net",
-    Version = "0.0.1",
-    Description = "Resume.OrchardCore"
+    Version = "1.0.0-beta3",
+    Description = "Resume theme"
 )]
